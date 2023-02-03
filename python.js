bf021384017e2edc24383b76c57a596d30af77a6
Tulip <93089383+Tulip-0333@users.noreply.github.com>
2023-02-02 22:31:58 -0500

    Update python.js

--- python.js
+++ python.js
@@ -29,10 +29,74 @@ function range(start, end, step = 1) {
   return arr;
 }
 
-String.prototype.len = function() {
-  return this.length;
+function len(str) {
+  return str.length;
 };
 
 function sum(arr) {
   return arr.reduce((acc, cur) => acc + cur, 0);
 }
+
+function average(arr) {
+  return sum(arr) / arr.length;
+}
+
+function sort(arr, comparator = (a, b) => a - b) {
+  return arr.sort(comparator);
+}
+
+function max(arr) {
+  return Math.max(...arr);
+}
+
+function min(arr) {
+  return Math.min(...arr);
+}
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+function slice(arr, start, end) {
+  return arr.slice(start, end);
+}
+
+function count(arr, element) {
+  return arr.reduce((count, cur) => count + (cur === element), 0);
+}
+
+function str(obj) {
+  return obj.toString();
+}
+
+function int(value) {
+  return parseInt(value);
+}
+
+function float(value) {
+  return parseFloat(value);
+}
+
+function type(obj) {
+  return typeof obj;
+}
+
+
+module.exports = {
+  print,
+  input,
+  range,
+  len,
+  sum,
+  average,
+  sort,
+  max,
+  min,
+  reverse,
+  slice,
+  count,
+  str,
+  int,
+  float,
+  type
+};
